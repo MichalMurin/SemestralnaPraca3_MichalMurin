f6bd2c19089e9faf945417c495a72f01720197e3
MichalMurin <murin.michal.945@gmail.com>
2023-05-10 09:27:35 +0200

    To string for customer updated

--- AgentSTKSimulation/StkStation/Models/Customer.cs
+++ AgentSTKSimulation/StkStation/Models/Customer.cs
@@ -12,6 +12,7 @@ namespace DISS_S2.StkStation
     public class Customer: INotifyPropertyChanged
     {
         public double StartWaitingTime { get; set; }
+        private DateTime _startTimeForDebug;
         public bool HasParkingReserved { get; set; }
         public CarType CarType { get; set; }
         public int Id { get; set; }
@@ -33,6 +34,7 @@ namespace DISS_S2.StkStation
         public Customer(double pStartWaitingTime, CarType carType, int id)
         {
             StartWaitingTime = pStartWaitingTime;
+            _startTimeForDebug = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day).AddHours(9).AddSeconds(StartWaitingTime);
             CarType = carType;
             Id = id;
             Situation = CustomerSituation.CREATED;
@@ -56,7 +58,7 @@ namespace DISS_S2.StkStation
 
         public override string ToString()
         {
-            return $"Zakaznik {Id}, s autom {CarType} {CustomerSitutationService.SituationToString(Situation)}";
+            return $"Zakaznik {Id}, {CarType} {CustomerSitutationService.SituationToString(Situation)} ({_startTimeForDebug.ToString("HH:mm:ss")})";
         }
     }
 }
